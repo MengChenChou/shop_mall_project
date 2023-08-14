493ab8c34452b08daba02bcef705d968731534ac
MengChenChou <chou53175317@gmail.com>
2023-08-14 15:59:23 +0800

    init commit

---
+++ src/main/java/com/idv/shop_mall_project/ShopMallProjectApplication.java
@@ -0,0 +1,13 @@
+package com.idv.shop_mall_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMallProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopMallProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idv/shop_mall_project/ShopMallProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idv.shop_mall_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMallProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
