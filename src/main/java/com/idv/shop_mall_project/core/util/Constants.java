4d482e051fe19e9d995b13f0488f742d2c036c79
MengChenChou <chou53175317@gmail.com>
2023-08-20 15:16:19 +0800

    設置公用檔

--- src/main/java/com/idv/shop_mall_project/core/util/Constants.java
+++ src/main/java/com/idv/shop_mall_project/core/util/Constants.java
@@ -1,5 +1,12 @@
-package com.idv.shop_mall_project.core.util;/**
-@author RickChou
-@create 2023-08-20 下午 02:39
-*/public class Constants {
+package com.idv.shop_mall_project.core.util;
+
+/**
+ * @author RickChou
+ * @create 2023-08-20 下午 02:39
+ */
+public class Constants {
+    public final static String FILE_UPLOAD_DIC = "D:\\idea_springboot_mall_workspace\\images\\goods-img\\";
+
+
+
 }
