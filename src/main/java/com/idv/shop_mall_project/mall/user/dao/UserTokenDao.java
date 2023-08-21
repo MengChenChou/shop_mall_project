67f591e2ca974f5c7bcdd67a15e2419c58c56a47
MengChenChou <chou53175317@gmail.com>
2023-08-21 20:37:34 +0800

    dao

---
+++ src/main/java/com/idv/shop_mall_project/mall/user/dao/UserTokenDao.java
@@ -0,0 +1,8 @@
+package com.idv.shop_mall_project.mall.user.dao;
+
+/**
+ * @author RickChou
+ * @create 2023-08-21 下午 07:21
+ */
+public interface UserTokenDao {
+}
---
+++ src/main/java/com/idv/shop_mall_project/mall/user/dao/impl/UserTokenDaoImpl.java
@@ -0,0 +1,8 @@
+package com.idv.shop_mall_project.mall.user.dao.impl;
+
+/**
+ * @author RickChou
+ * @create 2023-08-21 下午 07:23
+ */
+public class UserTokenDaoImpl {
+}
